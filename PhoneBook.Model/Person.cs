8710cb11319259934d7885a5a510d2b2cdc40e05
Efremov Mikhail <terminal7@dtn.ru>
2025-02-09 17:03:40 +0300

    Delete WebApi

--- PhoneBook.Model/Person.cs
+++ PhoneBook.Model/Person.cs
@@ -6,6 +6,6 @@ public class Person
     public string FirstName { get; set; }
     public string LastName { get; set; } = string.Empty;
     public string Patronymic { get; set; } = string.Empty;
-    public List<Phone> Phones { get; set; } = [];
     public bool IsDeleted { get; set; } = false;
+    public List<Phone> Phones { get; set; } = [];
 }
