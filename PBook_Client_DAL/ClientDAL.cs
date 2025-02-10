92b5f3051ccebe0e388f1711859f6127055e388c
Krivo_kv02@top-academy.ru <Krivo_kv02@top-academy.ru>
2025-02-10 21:05:41 +0300

    создала клиентДАЛ, вебАрр

---
+++ PBook_Client_DAL/ClientDAL.cs
@@ -0,0 +1,35 @@
+
+using PBook_Model;
+using System.Collections.ObjectModel;
+using System.Net.Http;
+using System.Net.Http.Json;
+
+namespace PBook_Client_DAL
+{
+    public class ClientDAL
+    {
+        private static readonly HttpClient Client = new();
+
+        public ObservableCollection<Book> books;
+        public ClientDAL()
+        {
+            books = new ObservableCollection<Book>();
+        }
+
+        public async Task<IEnumerable<Book>> Dal_GetAllBooks_Async() =>
+            await Client.GetFromJsonAsync<IEnumerable<Book>>
+            (new Uri($"http://localhost:5182/books/"));
+
+        public async Task<Book> Dal_GetBookById(int id) =>
+            await Client.GetFromJsonAsync<Book>
+            (new Uri($"http://localhost:5182/books/{id}"));
+
+        public async Task Dal_AddBook(string first_name, string last_name, string patronymic, int type_id, string number) =>
+            await Client.PostAsJsonAsync(new Uri($"http://localhost:5182/book/{first_name},{last_name},{patronymic},{type_id},{number}"),
+                (first_name, last_name, patronymic, type_id, number));
+
+        public async Task Dal_DeleteBook(int id) =>  await Client.DeleteAsync(new Uri($"{id}"));
+        
+    }
+
+}
---
+++ WebApp/Program.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Builder;
+using PBook_BL;
+
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+app.UseHttpsRedirection();
+var service = new Service();
+
+app.MapGet("/books", async () => await service.GetAllBook_Async());
+
+app.MapGet("/book/{id}", async(int id) => await service.GetBookById_Async(id));
+
+app.MapPost("/book/{first_name},{last_name},{patronymic},{type_id},{number}",
+    async (string first_name, string last_name, string patronymic, int type_id, string number) =>
+    await service.AddBook_Async(first_name, last_name, patronymic, type_id, number));
+
+app.MapDelete("/book/{id}", async(int id)=> await service.DeleteBook_Async(id));
+
+
+app.Run();
+
+
+
+
