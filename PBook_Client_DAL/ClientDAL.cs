20d5a764c85f8f97288c1d1ed60d623b57bc0784
ShizoTempest <83418225+ShizoTempest@users.noreply.github.com>
2025-02-11 21:38:49 +0300

    test_Vova

--- PBook_Client_DAL/ClientDAL.cs
+++ PBook_Client_DAL/ClientDAL.cs
@@ -11,6 +11,8 @@ namespace PBook_Client_DAL
         private static readonly HttpClient Client = new();
 
         public ObservableCollection<Book> books;
+        public ObservableCollection<Person> persons; // добавил коллекцию персон
+        public ObservableCollection<PhoneType> phoneTypes;
         public ClientDAL()
         {
             books = new ObservableCollection<Book>();
@@ -29,8 +31,34 @@ namespace PBook_Client_DAL
                 (first_name, last_name, patronymic, type_id, number));
 
         public async Task Dal_DeleteBook(int id) =>  await Client.DeleteAsync(new Uri($"{id}"));
-        // comment edit
-        
+
+        // Добавил заглушки
+
+        public async Task<IEnumerable<Person>> Dal_GetAllPerson_Async() =>
+            await Client.GetFromJsonAsync<IEnumerable<Person>>
+            (new Uri($"http://localhost:5182/persons/"));
+
+        public async Task<Person> Dal_GetPersonById_Async(int id) =>
+            await Client.GetFromJsonAsync<Person>
+            (new Uri($"http://localhost:5182/person/{id}"));
+
+        public async Task<IEnumerable<PhoneType>> Dal_GetAllPhoneType_Async() =>
+            await Client.GetFromJsonAsync<IEnumerable<PhoneType>>
+            (new Uri($"http://localhost:5182/phoneTypes/"));
+
+        public async Task<PhoneType> Dal_GetPhoneTypeById_Async(int id) =>
+            await Client.GetFromJsonAsync<PhoneType>
+            (new Uri($"http://localhost:5182/phoneType/{id}"));
+
+        public async Task Dal_AddPerson(string first_name, string last_name, string patronymic) =>
+            await Client.PostAsJsonAsync(new Uri($"http://localhost:5182/person/{first_name},{last_name},{patronymic}"),
+                (first_name, last_name, patronymic));
+
+        public async Task Dal_DeletePerson_Async(int id) => await Client.DeleteAsync(new Uri($"{id}"));
+
+        public async Task<int> Dal_GetPersonIdByFullName(string first_name, string last_name, string patronymic) =>
+            await Client.GetFromJsonAsync<int>
+            (new Uri($"http://localhost:5182/person/{first_name}, {last_name}, {patronymic}"));
     }
 
 }
--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -16,8 +16,26 @@ app.MapPost("/book/{first_name},{last_name},{patronymic},{type_id},{number}",
     await service.AddBook_Async(first_name, last_name, patronymic, type_id, number));
 
 app.MapDelete("/book/{id}", async(int id)=> await service.DeleteBook_Async(id));
-//comment edit
 
+// Добавил заглушки
+
+app.MapGet("/persons", async () => await service.GetAllPerson_Async());
+
+app.MapGet("/person/{id}", async (int id) => await service.GetPersonById_Async(id));
+
+app.MapGet("/phoneTypes", async () => await service.GetAllPhoneType_Async());
+
+app.MapGet("/phoneType/{id}", async (int id) => await service.GetPhoneTypeById_Async(id));
+
+app.MapPost("/person/{first_name},{last_name},{patronymic}", 
+    async (string first_name, string last_name, string patronymic) => 
+    await service.AddPerson(first_name, last_name, patronymic));
+
+app.MapDelete("/person/{id}", async (int id) => await service.DeletePerson_Async(id));
+
+app.MapGet("/person/{first_name},{last_name},{patronymic}", 
+    async(string first_name, string last_name, string patronymic) => 
+    await service.GetPersonIdByFullName(first_name, last_name, patronymic));
 
 app.Run();
 
