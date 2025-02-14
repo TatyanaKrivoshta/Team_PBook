736b685e46ec9c6c617e9408b44482fef410a3f0
EMIHotCode <163670522+EMIHotCode@users.noreply.github.com>
2025-02-14 22:20:37 +0300

    Добавлен NLog.config в PBook_ClientDAL
    В WPF реализовано логирование в
    конструкторе основываясь на PBook_Client_DAL

--- Team_PBook/Model/ClassTest.cs
+++ Team_PBook/Model/ClassTest.cs
@@ -4,29 +4,40 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using PBook_BL;
+using PBook_Client_DAL;
 using PBook_Model;
 
 namespace Team_PBook_wpf.Model
 {
     public class ClassTest
     {
-        public Service service;
+        public ClientDAL clientDal;
+        private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
         public ObservableCollection<Person> persons= [];
         public ObservableCollection<Book> books=[];
         public ObservableCollection<PhoneType> phoneTypes=[];
         public ClassTest()
         {
-            service = new Service();
-            Test_function();
+            clientDal = new ClientDAL();
+            try
+            {
+                Test_function();
+                Logger.Info("Коллекции БД загрузились с сервера");
+            }
+            catch (Exception e)
+            {
+                Logger.Fatal(e, "Коллекции базы данных НЕ ЗАГРУЗИЛИСЬ с сервера");
+                throw;
+            }
+            
         }
         public async Task Test_function()
         {
-            persons = new ObservableCollection<Person>( await service.GetAllPerson_Async());
+            persons = new ObservableCollection<Person>( await clientDal.Dal_GetAllPerson_Async());
             int c=persons.Count();
-            books = new ObservableCollection<Book>(await service.GetAllBook_Async());
+            books = new ObservableCollection<Book>(await clientDal.Dal_GetAllBooks_Async());
             int d=books.Count();
-            phoneTypes = new ObservableCollection<PhoneType>(await service.GetAllPhoneType_Async());
+            phoneTypes = new ObservableCollection<PhoneType>(await clientDal.Dal_GetAllPhoneType_Async());
             int e=phoneTypes.Count();
         }
     }
